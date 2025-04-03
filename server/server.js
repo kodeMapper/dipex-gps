a5348915bfb77acae45db62d9fdfe7dcdb151f58
kodeMapper <thesaranggade@gmail.com>
2025-04-03 07:08:50 +0530

    add deployed link

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://your-vercel-app-name.vercel.app'],
+  origin: ['http://localhost:3000', 'https://dipex-gps.vercel.app'],
   credentials: true
 }));
 
